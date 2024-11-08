1a2478f990e8dc37e9df1ab4b4348ce1efd6fd79
SantiagoF311 <112028544+SantiagoF311@users.noreply.github.com>
2024-11-08 17:13:30 -0500

    Update authMiddleware.js

--- authMiddleware.js
+++ authMiddleware.js
@@ -5,18 +5,24 @@ import User from './models/person/person.js';
 export const authenticateUser = async (req, res, next) => {
   const token = req.headers['authorization'];
 
-  if (!token) {
-    return res.status(401).json({ error: 'Token no proporcionado' });
+  if (!token || !token.startsWith('Bearer ')) {
+    return res.status(401).json({ error: 'Token no proporcionado o formato incorrecto' });
   }
 
+  const actualToken = token.split(' ')[1];
+
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id);
+    const decoded = jwt.verify(actualToken, process.env.JWT_SECRET);
+    req.user = decoded;
     next();
   } catch (error) {
+    if (error.name === 'TokenExpiredError') {
+      return res.status(401).json({ error: 'Token ha expirado' });
+    }
     return res.status(403).json({ error: 'Token inválido' });
   }
 };
+
 
 export const isAdmin = (req, res, next) => {
   const user = req.user;
