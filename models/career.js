ca0f942c6da412be75657070a4e4c968c88fc1c3
SantiagoF311 <112028544+SantiagoF311@users.noreply.github.com>
2024-11-09 13:32:41 -0500

    Update career.js

--- models/career.js
+++ models/career.js
@@ -1,9 +1,10 @@
-import mongoose from 'mongoose';
-
 const careerSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   description: { type: String },
-  classes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Class' }] // Cambiado para incluir clases asociadas a la carrera
+  classes: [{
+    classId: { type: mongoose.Schema.Types.ObjectId, ref: 'Class' },
+    semesters: [{ type: Number }]  // Lista de semestres asociados a la clase
+  }]
 }, { timestamps: true });
 
 const Career = mongoose.model('Career', careerSchema);
